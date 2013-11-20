b26982017d9e17bc1a435c3e74ccdaf0bce5427d
Chris Prosser <ProsserCJ1@gcc.edu>
2013-11-20 14:46:28 -0500

    Added floating number damage indicators

--- lifeform.cpp
+++ lifeform.cpp
@@ -8,12 +8,12 @@ void Lifeform::draw()
 	Mover::draw();
 	if (lostHealth > 0)
 	{
-		if (++fontDisplayFrameCount < 40)
+		if (++fontDisplayFrameCount < 80)
 		{
 			char buffer[5];
 			sprintf(buffer,"-%i",lostHealth);
-			gameFont->setFontColor(SETCOLOR_ARGB(255 - 6*fontDisplayFrameCount,255,0,0));
-			gameFont->print(buffer, getX()+getWidth()/2-5, getY() - .25*fontDisplayFrameCount);		
+			gameFont->setFontColor(SETCOLOR_ARGB(255 - 3*fontDisplayFrameCount,255,0,0));
+			gameFont->print(buffer, getX()+getWidth()/2-5, getY() - .125*fontDisplayFrameCount);		
 		}
 		else{
 			fontDisplayFrameCount = 0;
