c8ab4ebca484e52b8a04a4c5a3e4755fc4518286
Jake Gearhart <gearhartjj1@gcc.edu>
2013-12-04 23:14:35 -0500

    comment

--- statue.h
+++ statue.h
@@ -1,3 +1,11 @@
+/* 
+ Group #: Team 3 
+ Members: Chris Prosser, Jacob Gearhart, Kory Kappel, and Andy Miller
+ Course: COMP 322, Advanced Programming 
+ Date: 28 October 2013 
+ Description: this is the header file for the statue class
+ */
+
 #ifndef __STATUE_H
 #define __STATUE_H
 
